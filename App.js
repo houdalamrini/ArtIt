7214ff227046cfd74ce493fadcda5b87ffbdf0a7
houdalamrini <103292007+houdalamrini@users.noreply.github.com>
2022-05-06 12:56:42 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button,TextInput,TouchableOpacity, StyleSheet,Platform,Text,Image, TouchableWithoutFeedbackComponent, View,SafeAreaView } from 'react-native';
+import WelcomeScreen from './app/assets/screns/WelcomeScreen';
+export default function App() {
+    
+    return<WelcomeScreen/>
+    
+    }
